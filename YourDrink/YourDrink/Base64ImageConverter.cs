cb7e195d316436ff6d7fc0762ca79e020b0524a0
cf <preyde@pascals-mbp.speedport_w_724v_typ_a_05011603_06_003>
2020-03-12 20:51:56 +0100

    new

---
+++ YourDrink/YourDrink/Base64ImageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Xamarin.Forms;
+
+namespace YourDrink
+{
+    public class Base64ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var base64 = (string)value;
+
+            if (base64 == null)
+                return null;
+
+            return ImageSource.FromStream(() =>
+            {
+                return new MemoryStream(System.Convert.FromBase64String(base64));
+            });
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
+
---
+++ YourDrink/YourDrink/MasterDetail.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite;
+using YourDrink.Model;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific.AppCompat;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace YourDrink
+{
+    public partial class MasterDetail : MasterDetailPage
+    {
+        public static MasterDetail that { get; set; }
+        public static ToolbarItem MainItem { get { return MainItem; } set => that.MainToolbarItem = MainItem; }
+
+        public MasterDetail()
+        {
+            InitializeComponent();
+
+            var page = new Xamarin.Forms.NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Xamarin.Forms.Color.FromHex("#4d0018")
+            };
+            page.On<Android>().SetBarHeight(120);
+
+
+
+            masterDetailPage.Detail = page;
+            MainItem = MainToolbarItem;
+            that = this;
+            
+        }
+
+        public void OpenSideBar()
+        {
+            masterDetailPage.IsPresented = true;
+        }
+
+       public void AddCategory(System.Object sender, System.EventArgs e)
+        {
+            CategoryListPage.That.AddCategory(sender, e);
+        }
+    }
+}
